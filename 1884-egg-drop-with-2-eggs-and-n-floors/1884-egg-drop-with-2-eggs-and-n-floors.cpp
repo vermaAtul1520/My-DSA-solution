faf48ac73a2a0f6cfba5a493aa6cbca8a8552844
Atul verma <atulverma1520@gmail.com>
2023-03-31 17:41:26 +0530

    Time: 122 ms (26.74%), Space: 7.2 MB (17.78%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int solve(int e,int n,vector<vector<int>> &dp){
-        if(n==0 || n==1) return n;
-        if(e==1) return n;
-        
-        if(dp[e][n]!=-1) return dp[e][n];
-        
-        int tmp=INT_MAX;
-        for(int i=1;i<=n;i++){
-            int nbreak=solve(e,n-i,dp);
-            int bk=INT_MIN;
-            if(e==2) bk=solve(1,i-1,dp);
-            tmp=min(tmp,1+max(bk,nbreak));
-        }
-        return dp[e][n]=tmp;
-    }
+    
     int twoEggDrop(int n) {
-        vector<vector<int>>dp(3,vector<int>(n+1,-1));
-        return solve(2,n,dp); 
+        vector<vector<int>>dp(3,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++) dp[1][i]=i;
+        dp[1][1]=1;
+        dp[2][1]=1;
+        for(int t=2;t<=n;t++){
+            int tmp=INT_MAX;
+            for(int i=2;i<=t;i++){
+                int nbreak=dp[2][t-i];
+                int bk=dp[1][i-1];
+                tmp=min(tmp,1+max(bk,nbreak));
+            }
+            dp[2][t]=tmp;
+        }
+        return dp[2][n];
     }
 };
