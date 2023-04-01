3865ba5ba897e1c50e9353f12897113087d8a87d
Atul verma <atulverma1520@gmail.com>
2023-04-01 16:41:14 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -5,15 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int solve(int r,int c,vector<vector<int>>& triangle,int n,vector<vector<int>> &dp){
-        if(r==n-1) return triangle[r][c];
-        if(dp[r][c]!=-1) return dp[r][c];
+//   Recursion and memoizationn,......
+
+    // int solve(int r,int c,vector<vector<int>>& triangle,int n,vector<vector<int>> &dp){
+    //     if(r==n-1) return triangle[r][c];
+    //     if(dp[r][c]!=-1) return dp[r][c];
         
-        return dp[r][c]=triangle[r][c]+min(solve(r+1,c,triangle,n,dp),solve(r+1,c+1,triangle,n,dp));
-    }
+    //     return dp[r][c]=triangle[r][c]+min(solve(r+1,c,triangle,n,dp),solve(r+1,c+1,triangle,n,dp));
+    // }
+    
+    
     int minimizeSum(int n, vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(0,0,triangle,n,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++) dp[n-1][i]=triangle[n-1][i];
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
 
